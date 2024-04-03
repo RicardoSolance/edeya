193f73156a03e114250afd4d60841446bbecb66d
Ricardo Solance <ricardontutumu@gmail.com>
2024-04-04 00:38:18 +0200

    ROUTING IMPLEMENTED

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,10 @@
 import "dotenv/config";
 import express from "express";
-// import usersRouter from "./routes/users";
 import connectToDataBase from "./config/mongoDB";
 import cors from "cors";
 import routing from "./routes/routing";
 
 const app = express();
-app.use(express.json());
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -14,10 +12,9 @@ app.use(express.urlencoded({ extended: true }));
 const PORT = process.env.PORT || 3000;
 
 connectToDataBase();
-// app.use("/api", usersRouter);
 routing(app);
 
 app.listen(PORT, () => {
-  console.log(`edeyá running on port ${PORT}`);
-  console.log(`http://${process.env.IP_SERVER}:${PORT}/api`);
+  console.log(`EDEYÁ SEVER RUNNING ON PORT ${PORT}`);
+  console.log(`http://${process.env.IP_SERVER}:${PORT}`);
 });
