5e4064721a0b6c40a9dea598f4d5024e03683b52
Ricardo Solance <ricardontutumu@gmail.com>
2024-08-06 15:50:44 +0200

    Changing job create function

--- src/controller/Job.ts
+++ src/controller/Job.ts
@@ -5,32 +5,61 @@ import Recruiter from "../model/Recruiter";
 import BadRequestError from "../errors/BadRequestError";
 import Job from "../model/Job";
 
+// export const createJob = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+//   try {
+//     const email = res.locals.email;
+//     const recruiterExist = await Recruiter.findOne(
+//       { email },
+//       {
+//         company: 1,
+//         _id: 1,
+//       }
+//     );
+//     if (recruiterExist) {
+//       const newJob = new Job({
+//         ...req.body,
+//         recruiterId: recruiterExist.id,
+//         jobId: uuidv4(),
+//         companyId: recruiterExist.company,
+//       });
+//       await newJob.save();
+//       res.json({ message: "New job published" });
+//     } else {
+//       throw new BadRequestError("This recruiter is not active");
+//     }
+
+//     res.json(req.body);
+//   } catch (error) {
+//     next(error);
+//   }
+// };
+
+// Función para manejar errores
+const handleError = (error: Error, next: NextFunction) => {
+  next(error);
+};
+
 export const createJob = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   try {
     const email = res.locals.email;
-    const recruiterExist = await Recruiter.findOne(
-      { email },
-      {
-        company: 1,
-        _id: 1,
-      }
-    );
-    if (recruiterExist) {
-      const newJob = new Job({
-        ...req.body,
-        recruiterId: recruiterExist.id,
-        jobId: uuidv4(),
-        companyId: recruiterExist.company,
-      });
-      await newJob.save();
-      res.json({ message: "New job published" });
-    } else {
+
+    const recruiter = await Recruiter.findOne({ email }, { company: 1, _id: 1 });
+
+    if (!recruiter) {
       throw new BadRequestError("This recruiter is not active");
     }
 
-    res.json(req.body);
+    const newJob = new Job({
+      ...req.body,
+      recruiterId: recruiter._id,
+      jobId: uuidv4(),
+      companyId: recruiter.company,
+    });
+
+    await newJob.save();
+    res.json({ message: "New job published" });
   } catch (error) {
-    next(error);
+    handleError(error, next);
   }
 };
 
